3eda42d5e43178187651f965ef1d49d53a2cfd00
maltsevdm <dimamalcev777@gmail.com>
2024-12-13 17:59:52 +0500

    add to_exe.bat

---
+++ to_exe.bat
@@ -0,0 +1,12 @@
+python3 -m venv .venv
+.venv\Scripts\activate
+pip install -r requirements.txt
+
+pyinstaller \
+    --noconfirm \
+    --onefile \
+    -w \
+    --add-data "svod_parser_v1:svod_parser_v1/" \
+    --add-data "svod_parser_v2:svod_parser_v2/" \
+    -n "Парсер СВОД" \
+    "app.py"
