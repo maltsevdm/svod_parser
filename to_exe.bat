e5512fe1018076cd64ea8a59826cad267357bcee
maltsevdm <dimamalcev777@gmail.com>
2024-12-23 21:16:14 +0500

    update too_exe.bat

--- to_exe.bat
+++ to_exe.bat
@@ -1,12 +1,3 @@
-python3 -m venv .venv
+python -m venv .venv
 .venv\Scripts\activate
-pip install -r requirements.txt
-
-pyinstaller \
-    --noconfirm \
-    --onefile \
-    -w \
-    --add-data "svod_parser_v1:svod_parser_v1/" \
-    --add-data "svod_parser_v2:svod_parser_v2/" \
-    -n "Svod parser" \
-    "app.py"
+pip install -r requirements.txt && pyinstaller --noconfirm --onefile -w --add-data "svod_parser_v1:svod_parser_v1/" --add-data "svod_parser_v2:svod_parser_v2/" -n "Svod parser" "app.py"
